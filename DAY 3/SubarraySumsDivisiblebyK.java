b23f0de2edd3b238c4193b7026e5850660108f91
ndhineshkumaar <ndhineshkumaar@gmail.com>
2024-03-19 15:25:29 +0530

    day 3 final

--- DAY 3/SubarraySumsDivisiblebyK.java
+++ DAY 3/SubarraySumsDivisiblebyK.java
@@ -1,17 +1,19 @@
+import java.util.HashMap;
+
 public class SubarraySumsDivisiblebyK {
     public static void main(String[] args) {
         int[] nums = {4, 5, 0, -2, -3, 1};
         int k = 5;
-        int count = 0;
-        for (int i = 0; i < nums.length; i++) {
-            int sum = 0;
-            for (int j = i; j < nums.length; j++) {
-                sum += nums[j];
-                if (sum % k == 0) {
-                    count++;
-                }
-            }
+        HashMap<Integer,Integer> map=new HashMap<>();
+        int sum=0,count=0;
+        map.put(0,1);
+        for(int i:nums){
+            sum+=i;
+            int rem=sum%k;
+            if(rem<0) rem+=k;
+            if(map.containsKey(rem)) count+=map.get(rem);
+            map.put(rem,map.getOrDefault(rem,0)+1);
         }
-        System.out.println(count);
+        System.out.println(map);
     }
 }
